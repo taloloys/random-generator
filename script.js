3969a33eefa3734f6c95d0e4b8b73e12b548df16
taloloys <klenthprog@gmail.com>
2024-09-27 20:13:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,97 +1,73 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.registration-link');
-const btnPop = document.querySelector('.btnLogin-popup');
-const iconClose = document.querySelector('.close-icon');
+let dogz = document.getElementById("dogImage");
+let btn = document.getElementById("btn1");
 
-registerLink.addEventListener('click', ()=> {
-    wrapper.classList.add('active');
-})
+btn.addEventListener('click',randomDogImage);
+const dogUrl = "https://dog.ceo/api/breeds/image/random";
 
-loginLink.addEventListener('click', ()=> {
-    wrapper.classList.remove('active');
-})
 
-btnPop.addEventListener('click', ()=> {
-    wrapper.classList.add('active-popup');
+function randomDogImage() {
+    fetch(dogUrl)
+    .then(response => response.json())
+    .then(data => {
+        dogz.innerHTML = `<img src ="${data.message}" width = "290" height = "290" class='dog-image'/>`;
+    })
 
-})
-
-iconClose.addEventListener('click', ()=> {
-    wrapper.classList.remove('active-popup');
-})
-
-let modal = document.getElementById('popUp');
-
-function signUp() {
-    const username = document.getElementById('registerUsername').value;
-    const email = document.getElementById('registerEmail').value;
-    const password = document.getElementById('registerPassword').value;
-
-      if (localStorage.getItem(username)) {
-        alert('Username already exists. Please choose another.');
-        return;
-      }
-
-      localStorage.setItem(username, JSON.stringify({ username, email, password }));
-      openModal();
-      clearTanan();
-      
-      
 }
 
-document.getElementById("regForm").addEventListener("submit", signUp);
+
+let cats = document.getElementById("catImage");
+let bts = document.getElementById("btn2");
+
+bts.addEventListener('mouseover',randomCatImage);
+const apiKey = "live_SFcPx0u55f8gWdfvYaWJIakUWQOnU8UWtHDHyzr8ryPGWoIZxj9K7J1kGXZGEDnL";
+const catUrl = "https://api.thecatapi.com/v1/images/search";
+
+function randomCatImage() {
+    fetch(catUrl)
+    .then(response => response.json())
+    .then(data => {
+        cats.innerHTML = `<img src = "${data[0].url }" width = '280' height = '280' class= 'cat-image'/>`;
+    })
+}
 
 
-function openModal() {
-    modal.classList.add("open-modal");
-  }
+let qoute = document.getElementById("quote");
+let auth = document.getElementById("author");
 
-  function closeModal() {
-    modal.classList.remove("open-modal");  
-    wrapper.classList.remove('active');
-  }
+let btns = document.getElementById("btn3");
+btns.addEventListener("click",randomQuote);
 
+let qouteUrl = "https://api.quotable.io/random";
 
+function randomQuote() {
+    fetch(qouteUrl)
+    .then(response => response.json())
+    .then(data => { 
+         let limitedQuote = limitWordsPerLine(data.content, 7);
+         qoute.innerHTML = `<div class="quote-bubble"><p>${limitedQuote.replace(/\n/g, '<br>')}</p></div>`;
+         auth.innerHTML = `<div class="author-bubble">Author: ${data.author}</div>`;
+    })
+}
 
-function signIn(event){
-    event.preventDefault();
-
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    
-
-    var user = localStorage.getItem(username);
-
-    if (user == null) {
-        alert('Invalid username');
-    } else {
-        var data = JSON.parse(user);
-
-        if (username === data.username && password === data.password) {
-            window.location.href = "../random generator/activity.html";
-        } else {
-            alert('Incorrect username or password');
-        }
+function limitWordsPerLine(text, wordsPerLine) {
+    let words = text.split(' ');
+    let lines = [];
+    for (let i = 0; i < words.length; i += wordsPerLine) {
+        let lineWords = words.slice(i, i + wordsPerLine);
+        lines.push(lineWords.join(' '));
     }
+    return lines.join('<br>');
 }
-document.getElementById("loginForm").addEventListener('submit', signIn);
-
-
-function clearTanan(){
-    var username = document.getElementById('registerUsername').value = '';
-    var email = document.getElementById('registerEmail').value = '';
-    var password = document.getElementById('registerPassword').value = '';
-    
-}
-document.getElementById('closing').addEventListener('click',clearTanan);    
-
-function clearLogin(){
-    var username = document.getElementById('username').value = '';
-    var password = document.getElementById('password').value = '';
+function disableBackButton() {
+    window.history.pushState(null, '', window.location.href);
+    window.onpopstate = function () {
+        window.history.pushState(null, '', window.location.href);
+    };
 }
 
-function showDescription() {
-    var description = document.getElementById("description");
-    description.style.display = (description.style.display === "none" || description.style.display === "") ? "block" : "none";
+function logout(){
+    window.location.href = "../login/login.html";
+    disableBackButton();
+
 }
+document.getElementById("logout").addEventListener('click',logout);
